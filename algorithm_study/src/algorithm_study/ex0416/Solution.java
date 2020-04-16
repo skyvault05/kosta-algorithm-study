7ebe81645e81e2ec6f4536f8ccc690054ae12355
skyvault05 <kimzombie@hotmail.com>
2020-04-16 17:22:52 +0900

    0416

---
+++ algorithm_study/src/algorithm_study/ex0416/Solution.java
@@ -0,0 +1,26 @@
+package algorithm_study.ex0416;
+
+class Solution {
+    public int[] solution(int[] prices) {
+    	int len = prices.length;
+        int[] answer = new int[len];
+        
+        for(int i=0;i<len-1; i++){
+            for(int j=i+1; j<len; j++){
+                answer[i]++;
+                if(prices[i]>prices[j]) {
+                	break;
+                }
+            }
+        }
+        answer[len-1]=0;
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		Solution s = new Solution();
+		
+		int[] prices = {1, 2, 3, 2, 3};
+		s.solution(prices);
+	}
+}
